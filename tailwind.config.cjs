1d21e35a5994f9aac101f959d2488092d2ca6036
sigreer <simon@sidewayssystems.co.uk>
2024-07-31 00:31:26 +0100

    bunch of fixes

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,33 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,scss}',
-	'./node_modules/flowbite/**/*.js'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue,scss}',
+		'./node_modules/flowbite/**/*.js'
+	],
 	safelist: [
-		'blue1',
-		'blue2',
-		'blue3',
-		'purple1',
-		'purple2',
-		'purple3',
-		'green1',
-		'green2',
-		'green3',
-		'orange1',
-		'orange2',
-		'orange3',
-		'red1',
-		'red2',
-		'red3',
-		'yellow1',
-		'yellow2',
-		'yellow3',
-		'cyan1',
-		'cyan2',
-		'cyan3',
-		'lime1',
-		'lime2',
-		'lime3',
-	  ],
+		'blue1', 'blue2', 'blue3',
+		'purple1', 'purple2', 'purple3',
+		'green1', 'green2', 'green3',
+		'orange1', 'orange2', 'orange3',
+		'red1', 'red2', 'red3',
+		'yellow1', 'yellow2', 'yellow3',
+		'cyan1', 'cyan2', 'cyan3',
+		'lime1', 'lime2', 'lime3',
+		'pink1', 'pink2', 'pink3',
+	],
 	theme: {
 		extend: {
 			maxWidth: {
@@ -37,22 +24,11 @@ module.exports = {
 		},
 		screens: {
 			'sm': '640px',
-			// => @media (min-width: 640px) { ... }
-	  
 			'md': '768px',
-			// => @media (min-width: 768px) { ... }
-	  
 			'lg': '1024px',
-			// => @media (min-width: 1024px) { ... }
-	  
 			'xl': '1280px',
-			// => @media (min-width: 1280px) { ... }
-	  
 			'2xl': '1536px',
-			// => @media (min-width: 1536px) { ... }
-			
 			'3xl': '1920px',
-			// => @media (min-width: 1920px) { ... }
 		},
 	},
 	plugins: [
